8089375cd2a59d22574a11ba40639030a4df1364
littletarzan <mdelore.ufl@gmail.com>
2026-02-28 20:49:41 -0500

    more verbose reverts transfer libraries

--- contracts/base/PLEXPairVault.sol
+++ contracts/base/PLEXPairVault.sol
@@ -21,8 +21,14 @@ abstract contract ReentrancyGuard {
 
 library SafeERC20 {
     function safeTransfer(IERC20 t, address to, uint256 v) internal {
-        bool ok = t.transfer(to, v);
-        require(ok, "TRANSFER_FAILED");
+        (bool success, bytes memory ret) =
+            address(t).call(
+                abi.encodeWithSelector(IERC20.transfer.selector, to, v)
+            );
+        require(success, "TRANSFER_CALL_FAILED");
+        if (ret.length > 0) {
+            require(abi.decode(ret, (bool)), "TRANSFER_FAILED");
+        }
     }
     function safeTransferFrom(
         IERC20 t,
@@ -30,8 +36,14 @@ library SafeERC20 {
         address to,
         uint256 v
     ) internal {
-        bool ok = t.transferFrom(from, to, v);
-        require(ok, "TRANSFER_FROM_FAILED");
+        (bool success, bytes memory ret) =
+            address(t).call(
+                abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, v)
+            );
+        require(success, "TRANSFER_FROM_CALL_FAILED");
+        if (ret.length > 0) {
+            require(abi.decode(ret, (bool)), "TRANSFER_FROM_FAILED");
+        }
     }
 }
 
